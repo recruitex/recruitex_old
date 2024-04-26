fb2b1d8098b9ad1a66d68c8c50c38e4f352a133e
Stanisław Synowiec <stasyn1410@gmail.com>
2024-04-26 18:23:46 +0200

    fix: format

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,11 +2,11 @@ import styles from './page.module.css';
 import type { ReactElement } from 'react';
 
 const Home = (): ReactElement => {
-	return (
-		<main className={styles.main}>
-			<h1 className={styles.title}>Welcome to Recruitex!</h1>
-		</main>
-	);
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Welcome to Recruitex!</h1>
+    </main>
+  );
 };
 
 export default Home;

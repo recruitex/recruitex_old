f25853b629ccb3c3462a1735b301c9c287cd7f8e
Hixo <hixohixo32@gmail.com>
2024-04-29 20:44:02 +0200

    fix: add shadcn/ui config to tailwind

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,11 @@
 import type { ReactElement } from 'react';
+import { Button } from '@repo/ui/button';
 
 const Home = (): ReactElement => {
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-between p-4">
       <h1 className="text-4xl font-bold ">Welcome to Recruitex!</h1>
+      <Button variant="destructive">Ta rakieta</Button>
     </main>
   );
 };

f8b28fb8f472088529c97e815fc6d761c1b94158
Hixo <hixohixo32@gmail.com>
2024-04-29 14:42:26 +0200

    feat: add tailwind

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,9 @@
-import styles from './page.module.css';
 import type { ReactElement } from 'react';
 
 const Home = (): ReactElement => {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>Welcome to Recruitex!</h1>
+    <main className="min-h-screen bg-black text-white flex flex-col items-center justify-between p-4">
+      <h1 className="text-4xl font-bold ">Welcome to Recruitex!</h1>
     </main>
   );
 };
---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    '../../packages/ui/components/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+    '../../apps/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
